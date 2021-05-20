72e0e20a0b386c4b90e3b7169cc8fdcd0aa359a5
dragana1611 <dragana16111@gmail.com>
2021-05-20 20:55:52 +0200

    25_cas_Objekti-Nizovi_objekata

---
+++ 25_JS_Objekti-Nizovi objekata/blogs/script.js
@@ -0,0 +1,154 @@
+let blog={
+    title:"Funkcije",
+    likes: 21,
+    dislikes:1,
+};
+let blogs = [
+    {title:"Nizovi", likes:16, dislikes:5},
+    { title: "Grananje", likes: 14, dislikes: 0}
+];
+
+console.log(blogs);
+
+////////////////////////////////////
+
+let blog1 = {
+    title: "musaka od tikvica",
+    like:105,
+    dislikes:31
+};
+
+let blog2 = {
+    title: "lazanja!",
+    like: 236,
+    dislikes: 13
+};
+
+let blog3 = {
+    title: "zapecena boranija!",
+    like: 60,
+    dislikes: 32
+};
+
+let blogsArr = [blog1, blog2, blog3];
+// ispis objekata iz niza objekata
+blogsArr.forEach(elem=>{
+    console.log(elem);
+});
+
+//ispis naslova iz niza blogova forEach petljom
+console.log(blog1.title);
+blogsArr.forEach(elem=>{
+    console.log(elem.title);
+});
+
+//ispis naslova iz niza blogova for petljom
+
+console.log(blogsArr[0]);// vraca objekat na 0 poziziciji u nizu
+console.log(blogsArr[0].title); // vraca naslov objekta na 0 poziciji
+for(let i=0; i<blogsArr.length; i++){
+    console.log(blogsArr[i]);
+    //console.log(blogsArr[i].title);
+}
+
+/**Napisati foreach petlju koja ispisuje sve naslove iz niza objekata
+
+arrBlogs.forEach( blog=>{
+    console.log(blog.title);
+});
+
+Napraviti arrow funkciju kojoj se prosleđuje niz objekata, a ona vraća ukupan broj lajkova
+ */
+
+let sumaLajkova = nizObj => {
+    let ukupnoLajkova=0;
+nizObj.forEach(elem =>{
+    ukupnoLajkova += elem.like;
+});
+    return ukupnoLajkova;
+}
+console.log(sumaLajkova(blogsArr));
+
+//Napraviti arrow funkciju kojoj se prosleđuje niz objekata, a ona vraća prosečan broj lajkova
+let prosekLajkova = nizObj=> {
+    let sum = sumaLajkova(nizObj);
+    let brojac=nizObj.length;
+    return sum/brojac;
+}
+console.log(prosekLajkova(blogsArr));
+
+/**Napraviti arrow funkciju kojoj se prosleđuje niz objekata, a ona ispisuje sve one naslove blogova koji imaju više pozitivnih nego negativnih ocena
+  
+
+Napraviti arrow funkciju kojoj se prosleđuje niz objekata, a ona ispisuje sve one naslove blogova koji imaju najmanje duplo više pozitivnih nego negativnih ocena
+ */
+
+/*let dislikes = nizObj=>{
+    
+    let sumaDislikes=0;
+    nizObj.forEach(elem=>{
+        sumaDislikes += elem.dislikes;
+        
+    });
+    return sumaDislikes;
+}
+console.log(dislikes(blogsArr));
+*/
+
+let viseLikes = nizObj =>{
+    let lista = "<ul>";
+    nizObj.forEach(elem=>{
+        if(elem.like>elem.dislikes){
+            lista += `<li>${elem.title}</li>`
+            console.log(elem.title);
+        }
+    });
+    lista += "</ul>";
+    document.body.innerHTML += lista;
+}
+viseLikes(blogsArr);
+
+let viseLikes2 = nizObj => {
+
+    nizObj.forEach(elem => {
+        if (elem.like >=2* elem.dislikes) {
+            console.log(elem.title);
+        }
+    });
+}
+viseLikes2(blogsArr);
+
+//Napisati arrow funkciju kojoj se prosleđuje niz objekata a ona ispisuje sve naslove koji se završavaju uzvičnikom
+let rec = "Hello!";
+let poslednjiKarakter = rec[rec.length-1];
+console.log(poslednjiKarakter);
+
+let poslednjiKarakter1 = rec.slice(-1); //odseca deo stringa
+console.log(poslednjiKarakter1);
+
+let poslednjiKarakter2 = rec.charAt(rec.length-1); // vraca karakter na odredjenom indeksu
+console.log(poslednjiKarakter2);
+
+let poslednjiKarakter3 = rec.endsWith('!');
+console.log(poslednjiKarakter3);
+
+//Napisati arrow funkciju kojoj se prosleđuje niz objekata a ona ispisuje sve naslove koji se završavaju uzvičnikom
+let uzvicnik = nizObj => {
+    nizObj.forEach(elem => {
+        let naslov = elem.title;
+        //I nacin
+
+        //if(naslov[naslov.length-1] == "!"){
+        //    console.log(naslov);
+        //}
+
+        // II nacin
+        if(naslov.endsWith("!")){
+            console.log(naslov);
+        }
+    });
+}
+uzvicnik(blogsArr);
+console.log("Hello\tworld");
+console.log("Hello\nworld");
+
---
+++ 25_JS_Objekti-Nizovi objekata/users/script.js
@@ -0,0 +1,156 @@
+let blog1 = {
+    title:'HTML',
+    like:50,
+    dislikes:3
+};
+
+let blog2 = {
+    title: 'CSS',
+    like: 170,
+    dislikes: 105
+};
+
+let blog3 = {
+    title: 'JS',
+    like: 205,
+    dislikes: 110
+};
+
+let user1 = {
+    username: 'Stefan',
+    age: 31,
+    blogs:[blog1, blog2, blog3],
+    logBlogs: function (){
+        this.blogs.forEach(elem =>{
+            console.log(elem.title);
+        });
+    },
+    sumLikes: function(){
+        let sum = 0;
+        this.blogs.forEach(blog=>{
+            sum += blog.like;
+        });
+        return sum;
+    }
+};
+
+
+//U objektu user, napraviti metod koji ispisuje sve blogove korisnika 
+user1.logBlogs();
+
+
+//Napraviti niz korisnika gde svaki od korisnika sadrži niz blogova.Svaki blog u ovom nizu je objekat.
+
+let user2 = {
+    username: 'Jelena',
+    age: 26,
+    blogs: [blog2, blog3],
+    sumLikes: function () {
+        let sum = 0;
+        this.blogs.forEach(blog => {
+            sum += blog.like;
+        });
+        return sum;
+    }
+};
+
+let user3 = {
+    username: 'Milena',
+    age: 14,
+    blogs: [blog1],
+    sumLikes: function () {
+        let sum = 0;
+        this.blogs.forEach(blog => {
+            sum += blog.like;
+        });
+        return sum;
+    }
+};
+
+let users = [user1, user2, user3];
+
+//Ispisati imena onih autora koji imaju ispod 18 godina
+
+users.forEach(elem/**user*/=>{
+    let godine = elem.age;
+    if(godine<18){
+        console.log(elem.username);
+    }
+});
+
+//Ispisati naslove onih blogova koji imaju više od 50 lajkova
+
+users.forEach(elem =>{
+    let userBlogs = elem.blogs; //niz blogova tekuceg korisnika
+        userBlogs.forEach(blog=>{
+            if(blog.like > 50){
+            console.log(blog.title);
+            }
+            //console.log(blog.title, elem.username);
+        });
+});
+
+//3.Ispisati sve blogove autora čiji je username ’Stefan’
+
+users.forEach(elem =>{
+    let ime = elem.username;
+    if (ime == 'Stefan'){
+        elem.blogs.forEach(blog =>{
+            console.log(blog.title, elem.username);
+        });      
+    }
+});
+
+
+/**4.Ispisati imena onih autora koji imaju ukupno više od 100 lajkova za blogove koje su napisali
+
+5.Ispisati naslove onih blogova koji imaju natprosečan broj pozitivnih ocena
+
+6.Ispisati naslove onih blogova koji imaju natprosečan broj pozitivnih ocena i ispodprosečan broj negativnih ocena
+ */
+
+//4 I nacin
+
+users.forEach(elem =>{
+    let userBlogs = elem.blogs;
+    let sumLikes = 0;
+    userBlogs.forEach(blog =>{
+        sumLikes += blog.like;
+    });
+    if(sumLikes >100){
+        console.log(elem.username);
+    }
+});
+
+
+//4 II nacin
+
+users.forEach(user=>{
+    if(user.sumLikes()>100){
+        console.log(user.username);
+    }
+})
+
+//4. III nacin
+
+let sum = blogs => {
+    let s=0;
+    blogs.forEach(blog=>{
+        s+=blog.like;
+    });
+    return s;
+}
+users.forEach(user=>{
+    let userBlogs = user.blogs;
+    let likes = sum(userBlogs);
+    if(likes>100){
+        console.log(user.username);
+    }
+});
+
+
+//5.
+
+
+
+
